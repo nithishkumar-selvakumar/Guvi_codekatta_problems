afda6cd409d52d48eccca408b6d787c749fc8712
nithishkumar-selvakumar <46522768+nithishkumar-selvakumar@users.noreply.github.com>
2019-01-14 21:31:05 +0530

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,10 @@
+#nithish kumar
+q=int(input())
+li=[]
+li=list(map(int,input().split(" ")))
+li.sort()
+for i in range(len(li)):
+	if(i==len(li)-1):
+		print(li[i],end="")
+	else:
+		print(li[i],end=" ")
