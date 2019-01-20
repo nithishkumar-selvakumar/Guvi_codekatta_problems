88893c7c98d5ecd026cb969c52cb5b511d266c8b
nithishkumar-selvakumar <46522768+nithishkumar-selvakumar@users.noreply.github.com>
2019-01-20 10:58:41 +0530

    Create given _no_print_words.c

---
+++ given _no_print_words.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter the number:");
+	scanf("%d",&n);
+	switch(n)
+	{
+		case 1:
+		print("one");
+		break;
+		case 2:
+		print("two");
+		break;
+		case 3:
+		print("three");
+		break;
+		case 4:
+		print("four");
+		break;
+		case 5:
+		print("five");
+		break;
+		case 6:
+		print("six");
+		break;
+		case 7:
+		print("seveen");
+		break;
+		case 8:
+		print("eight");
+		break;
+		case 9:
+		print("nine");
+		break;
+		case 0:
+		print("zero");
+		break;
+		default:
+			printf("no such number");
+	}
+}
