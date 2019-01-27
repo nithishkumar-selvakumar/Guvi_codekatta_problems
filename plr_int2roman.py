4007800391c794254ba7dd17619f5188f1085f23
nithishkumar-selvakumar <46522768+nithishkumar-selvakumar@users.noreply.github.com>
2019-01-27 13:40:41 +0530

    Create plr_int2roman.py

---
+++ plr_int2roman.py
@@ -0,0 +1,9 @@
+# your code goes here
+#NITHISH
+x=input()
+n=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVII','XVIII','XIX','XX']
+a=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20']
+for i in range(len(n)):
+	if(x==n[i]):
+		print(a[i])
+		break
