e8326864d8da871e6dd024559da0ce987c2dc1eb
nithishkumar-selvakumar <46522768+nithishkumar-selvakumar@users.noreply.github.com>
2019-01-30 22:29:00 +0530

    Update hunter_set1_3.py

--- hunter_set1_3.py
+++ hunter_set1_3.py
@@ -1,9 +1,12 @@
-#hunter(3)
 #nithish
+#hunter
 n=int(input())
 l=list(map(int,input().split(" ")))
 a=[]
 for i in range(len(l)):
     if(l[i]==i):
         a.append(i)
-print(*a)        
+if len(a)>0:
+    print(*a)
+else:
+    print(-1)
